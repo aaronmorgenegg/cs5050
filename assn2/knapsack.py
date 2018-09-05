7884efd5b078b92a5b5c80f05ee914cdaac59731
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-04 19:30:13 -0600

    Initial work on knapsack homework

---
+++ assn2/knapsack.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Aaron Morgenegg, A02072659
+# Knapsack algorithm with 2 knapsacks
+
+def KnapRecursive(n, k1, k2, s):
+    """
+    Recursive solution, where:
+    n   == number of objects
+    k1  == size of knapsack 1
+    k2  == size of knapsack 2
+    s   == sizes of objects
+    """
+    pass
+
+def KnapMemo():
+    """
+    Memoized solution, where:
+    n   == number of objects
+    k1  == size of knapsack 1
+    k2  == size of knapsack 2
+    s   == sizes of objects
+    """
+    pass
+
