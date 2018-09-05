20c91d03aa623feeb8a863dc5b1a71f6da5762f6
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-04 20:02:01 -0600

    Added memoized knapsack algorithm and tests

--- assn2/knapsack.py
+++ assn2/knapsack.py
@@ -5,6 +5,8 @@
 # Knapsack algorithm with 2 knapsacks
 ########################
 
+cacheValid = {}
+
 def KnapRecursive(n, k1, k2, s):
     """
     Recursive solution, where:
@@ -21,7 +23,7 @@ def KnapRecursive(n, k1, k2, s):
             KnapRecursive(n-1, k1, k2, s)
             )
 
-def KnapMemo():
+def KnapMemo(n, k1, k2, s):
     """
     Memoized solution, where:
     n   == number of objects
@@ -29,7 +31,23 @@ def KnapMemo():
     k2  == size of knapsack 2
     s   == sizes of objects
     """
-    pass
+    try:
+        return cacheValid[(n, k1, k2)]
+    except KeyError:
+        pass
+    if k1 < 0 or k2 < 0: 
+        cacheValid[(n, k1, k2)] = False
+        return cacheValid[(n, k1, k2)]
+    if k1 is 0 and k2 is 0: 
+        cacheValid[(n, k1, k2)] = True
+        return cacheValid[(n, k1, k2)]
+    if n is 0: 
+        cacheValid[(n, k1, k2)] = False
+        return cacheValid[(n, k1, k2)]
+    return (KnapRecursive(n-1, k1-s[n-1], k2, s) or
+            KnapRecursive(n-1, k1, k2-s[n-1], s) or
+            KnapRecursive(n-1, k1, k2, s)
+            )
 
 ########################
 # Tests
@@ -41,7 +59,26 @@ def TestKnapRecursive(k1, k2, s, e):
     print("Expected result: {}".format(e))
     print("Actual result: {}\n".format(KnapRecursive(n, k1, k2, s)))
 
+def TestKnapMemo(k1, k2, s, e):
+    n = len(s)
+    print("\nRunning KnapMemo(n={}, k1={}, k2={}, s={})".format(n,k1,k2,s))
+    print("Expected result: {}".format(e))
+    print("Actual result: {}\n".format(KnapMemo(n, k1, k2, s)))
+
+
+print("Running simple tests of KnapRecursive and KnapMemo")
 
 TestKnapRecursive(4, 6, [4,3,3], "True")
-TestKnapRecursive(4, 6, [4, 3, 2, 2], "False")
+TestKnapRecursive(4, 6, [4,3,2,2], "False")
+
+TestKnapMemo(4, 6, [4,3,3], "True")
+TestKnapMemo(4, 6, [4,3,2,2], "False")
+
+print("Done running simple tests of KnapRecursive and KnapMemo")
+
+print("Running randomly generated tests comparing KnapRecursive and KnapMemo")
+
+
+print("Done running randomly generated tests comparing KnapRecursive and KnapMemo")
+
 
