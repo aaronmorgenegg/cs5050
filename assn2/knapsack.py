6eb29df7e27a85bbdf645b897163882f209055b4
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-04 20:40:37 -0600

    Added comments, runtime function

--- assn2/knapsack.py
+++ assn2/knapsack.py
@@ -2,11 +2,14 @@
 
 ########################
 # Aaron Morgenegg, A02072659
-# Knapsack algorithm with 2 knapsacks
+# Knapsack algorithms with 2 knapsacks, with 
+# both recursive and memoized algorithms
 ########################
 
 import random
+import timeit
 
+# Dictionary for memoizing data, indexed with a tuple (n, k1, k2)
 cacheValid = {}
 
 def KnapRecursive(n, k1, k2, s):
@@ -20,9 +23,9 @@ def KnapRecursive(n, k1, k2, s):
     if k1 < 0 or k2 < 0: return False
     if k1 is 0 and k2 is 0: return True
     if n is 0: return False
-    return (KnapRecursive(n-1, k1-s[n-1], k2, s) or 
-            KnapRecursive(n-1, k1, k2-s[n-1], s) or 
-            KnapRecursive(n-1, k1, k2, s)
+    return (KnapRecursive(n-1, k1-s[n-1], k2, s) or # Put it in knapsack 1
+            KnapRecursive(n-1, k1, k2-s[n-1], s) or # Put it in knapsack 2
+            KnapRecursive(n-1, k1, k2, s) # Don't put it in either knapsack
             )
 
 def KnapMemo(n, k1, k2, s):
@@ -33,12 +36,12 @@ def KnapMemo(n, k1, k2, s):
     k2  == size of knapsack 2
     s   == sizes of objects
     """
-    try:
+    try: # Try returning the memoized value, if it exists
         return cacheValid[(n, k1, k2)]
     except KeyError:
         pass
     if k1 < 0 or k2 < 0: 
-        cacheValid[(n, k1, k2)] = False
+        cacheValid[(n, k1, k2)] = False # Make sure to memoize the results
         return cacheValid[(n, k1, k2)]
     if k1 is 0 and k2 is 0: 
         cacheValid[(n, k1, k2)] = True
@@ -52,10 +55,11 @@ def KnapMemo(n, k1, k2, s):
             )
 
 ########################
-# Tests
+# Test Functions
 ########################
 
 def TestKnapRecursive(n, k1, k2, s, e):
+    """Helper function to test KnapRecursive function and print info"""
     print("\nRunning KnapRecursive(n={}, k1={}, k2={}, s={})".format(n,k1,k2,s))
     print("Expected result: {}".format(e))
     result = KnapRecursive(n, k1, k2, s)
@@ -70,6 +74,7 @@ def TestKnapMemo(n, k1, k2, s, e):
     return result
 
 def TestBothWithRandomData(repetitions):
+    """Compare results of both algorithms with random data, repeated of course"""
     for test in range(repetitions):
         print("\nRunning random test number {}\n".format(test))
         k1 = random.randint(0, 20)
@@ -83,6 +88,20 @@ def TestBothWithRandomData(repetitions):
         km = KnapMemo(n, k1, k2, s)
         if kr != km: print("ERROR: Recursive and Memoized solutions differ.")
         print("KnapRecursive={}\nKnapMemo={}\n".format(kr,km))
+
+def TimeFunction(function, *args):
+    """
+    Function to time the runtime of another function
+    Call with TimeFunction(function_name, arg1, arg2, ...,argn)
+    """
+    start_time = timeit.default_timer()
+    function(*args)
+    return timeit.default_timer() - start_time
+
+########################
+# Test Cases
+########################
+
 
 print("------------------------------------------")
 print("Running simple tests of KnapRecursive and KnapMemo")
