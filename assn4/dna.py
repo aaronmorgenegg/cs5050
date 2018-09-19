e56f014b09d0618d4fa8ff015945866c4154cdfd
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-19 13:43:59 -0600

    Initial commit for hw4 - dna matching

---
+++ assn4/dna.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+########################
+# Aaron Morgenegg, A02072659
+# String matching algorithm used 
+# to match DNA sequences
+########################
+
+DEBUG = True
+
+
+
