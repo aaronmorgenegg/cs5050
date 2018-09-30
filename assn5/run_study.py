51c700de6bf2e47e71d399a97e0aa814b195685c
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-09-29 19:27:29 -0600

    Added runstudy script to run iterated increasing study

---
+++ assn5/run_study.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+########################
+# Aaron Morgenegg, A02072659
+# Runs study of schoolbook algorthim
+# run with python run_study.py
+########################
+
+from polynomial import *
+
+mySolver = PolynomialSolver()
+mySolver.runStudy("schoolbook_data.txt")
+
