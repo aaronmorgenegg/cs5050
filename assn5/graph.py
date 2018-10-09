b51d85228a0ca52a743b6c0cac024bcfa6049749
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-10-09 12:07:05 -0600

    Log n vs log time

--- assn5/graph.py
+++ assn5/graph.py
@@ -18,7 +18,7 @@ def readData(filename):
 def graphData(data1, data2, graph_name):
     graph.plot(data1[0], data1[1], 'rx')
     graph.plot(data2[0], data2[1], 'bx')
-#    graph.xscale("log")
+    graph.xscale("log")
     graph.yscale("log")
     graph.xlabel("Problem Size (n)")
     graph.ylabel("Average Runtime (s)")
