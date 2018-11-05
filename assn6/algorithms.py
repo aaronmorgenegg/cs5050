197317e31728dea6ed9170c2ef854af651f396d5
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-05 14:57:18 -0700

    Reformat algorithm functions, add more constants options

--- assn6/algorithms.py
+++ assn6/algorithms.py
@@ -1,20 +1,19 @@
 
-class StringMatching:
-    def naive(self, text, pattern):
-        found_indices = []
-        n = len(text)
-        m = len(pattern)
-        for i in range(n-m+1):
-            for j in range(m):
-                if text[i+j] != pattern[j]:
-                    break
-                if j == m-1:
-                    found_indices.append(i)
-        return found_indices
+def naive(text, pattern):
+    found_indices = []
+    n = len(text)
+    m = len(pattern)
+    for i in range(n-m+1):
+        for j in range(m):
+            if text[i+j] != pattern[j]:
+                break
+            if j == m-1:
+                found_indices.append(i)
+    return found_indices
 
-    def KMP(self):
-        pass
+def KMP():
+    pass
 
-    def BM(self):
-        pass
+def BM():
+    pass
 
