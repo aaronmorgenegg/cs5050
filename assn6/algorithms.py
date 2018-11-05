9564e242b0121e13b3b9b5ac4b4eed3e9c583cf6
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-05 09:00:13 -0700

    Fixing bugs with naive algorithm

--- assn6/algorithms.py
+++ assn6/algorithms.py
@@ -1,20 +1,20 @@
 
 class StringMatching:
-    def naive(text, pattern):
+    def naive(self, text, pattern):
         found_indices = []
         n = len(text)
         m = len(pattern)
-        for i in range(n-m):
+        for i in range(n-m+1):
             for j in range(m):
                 if text[i+j] != pattern[j]:
                     break
-                if j == m:
+                if j == m-1:
                     found_indices.append(i)
         return found_indices
 
-    def KMP():
+    def KMP(self):
         pass
 
-    def BM():
+    def BM(self):
         pass
 
