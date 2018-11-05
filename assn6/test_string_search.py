35fa9dfe779ee4153b151e85bc1697198b508322
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-05 16:36:15 -0700

    Pretty print test matches

--- assn6/test_string_search.py
+++ assn6/test_string_search.py
@@ -1,14 +1,25 @@
 from binary_string_generator import *
 from algorithms import *
 
+def prettyPrintMatches(text, pat, matches):
+    print("Pattern  : {}".format(pat))
+    print("String   : {}".format(text))
+    string = ""
+    for i in range(len(text)):
+        if i in matches:
+            string += "^"
+        else:
+            string += " "
+    print("Matches  : {}".format(string))
+
 test_text1 = "ABRACADABRA"
 test_pat1 = "BRA"
 
 test1_naive = naive(test_text1, test_pat1)
-print(test1_naive)
+prettyPrintMatches(test_text1, test_pat1, test1_naive)
 
 test_text2 = "AABAACAADAABAAABAA"
 test_pat2 = "AABA"
 test2_naive = naive(test_text2, test_pat2)
-print(test2_naive)
+prettyPrintMatches(test_text2, test_pat2, test2_naive)
 
