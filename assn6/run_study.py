a4a538ee577eb4ba52209c911cedf86f4358858f
aaronmorgenegg <aaronmorgenegg@gmail.com>
2018-11-05 14:57:38 -0700

    Added run_study, which saves data from the different studies

---
+++ assn6/run_study.py
@@ -0,0 +1,65 @@
+import timeit
+
+from binary_string_generator import *
+from algorithms import *
+from constants import *
+
+ALGORITHMS = ["naive"]
+
+def TimeFunction(function, *args):
+    """
+    Function to time the runtime of another function
+    Call with TimeFunction(function_name, arg1, arg2, ...,argn)
+    """
+    start_time = timeit.default_timer()
+    function(*args)
+    return timeit.default_timer() - start_time
+
+def RunStudy():
+    runBinaryStudy()
+    runShakespeareStudy()
+    runDNAStudy()
+
+def runBinaryStudy():
+    if VERBOSITY > 1: print("Generating random binary string...")
+    binary_random = generateString(p=0.5)
+    for algorithm in ALGORITHMS:
+        iterateM(algorithm, binary_random, "binary_random")
+    del binary_random
+    if VERBOSITY > 1: print("Generating regular binary string...")
+    binary_regular = generateString(p=0.999)
+    for algorithm in ALGORITHMS:
+        iterateM(algorithm, binary_regular, "binary_regular")
+
+def runShakespeareStudy():
+    pass
+
+def runDNAStudy():
+    pass
+
+def lookupAlgorithm(algorithm):
+    if algorithm == "naive": return naive
+    if algorithm == "kmp": return kmp
+    if algorithm == "bm": return bm
+
+def iterateM(algorithm, text, study):
+    i = MIN_M
+    function = lookupAlgorithm(algorithm)
+    while i < MAX_M:
+        if VERBOSITY > 1: 
+            print("Running {} study with {} algorithm with m={}".format(study, algorithm, i))
+        pattern = text[-i:]
+        time = TimeFunction(function, text, pattern)
+        saveData(algorithm, study, i, time)
+        i *= 2
+
+def getFilename(algorithm, study):
+    return FILENAME_ROOT.format(study, algorithm)
+
+def saveData(algorithm, study, m, runtime):
+    filename = getFilename(algorithm, study)
+    with open(filename, "a") as myfile:
+        myfile.write(str(m)+","+str(runtime)+"\n")
+
+RunStudy()
+
